e6b7ce842673488c5b6d587d6cc1317f865071fc
Evan Farrar <evanfarrar@gmail.com>
2009-10-14 13:46:10 -0500

    Only apply migrations in shoes.

--- lib/opensprints-core.rb
+++ lib/opensprints-core.rb
@@ -7,10 +7,10 @@ require 'sequel/extensions/schema_dumper'
 require 'sqlite3'
 if(defined? Shoes) #Real environment
   DB = Sequel.connect("sqlite://#{DATABASE_PATH}")
+  Sequel::Migrator.apply(DB, 'lib/migrations/')
 else               #Test environment
   DB = Sequel.connect("sqlite::memory:")
 end
-Sequel::Migrator.apply(DB, 'lib/migrations/')
 Infinity = 1/0.0
 require 'opensprints-core/tournament'
 require 'opensprints-core/tournament_participation'
